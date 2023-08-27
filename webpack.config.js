dfe676b232b6ad9844640ac802ce45c09c4c2ff1
Iss1937 <133023027+Iss1937@users.noreply.github.com>
2023-08-27 22:50:53 +0300

    dz 4 need fix

--- webpack.config.js
+++ webpack.config.js
@@ -1,34 +1,51 @@
 const path = require("path");
 const HtmlWebpackPlugin = require("html-webpack-plugin");
+const MiniCssExtractPlugin = require("mini-css-extract-plugin");
 const CopyPlugin = require("copy-webpack-plugin");
+const CssMinimizerPlugin = require("css-minimizer-webpack-plugin");
+
 const isProduction = process.env.NODE_ENV === "production";
+
 module.exports = {
-    entry: "./src/cardGame.js",
+    entry: "./src/js/main.js",
     mode: isProduction ? "production" : "development",
     output: {
         path: path.resolve(__dirname, "dist"),
         filename: "bundle.js",
-        clean: "true",
+        clean: true,
     },
     module: {
         rules: [
-            { test: /\.css$/, use: ["style-loader", "css-loader"] },
             {
-                test: /\.(png|svg|jpg|jpeg|gif)$/i,
-                type: "asset/resource",
+                test: /\.scss$/i,
+                use: [
+                    MiniCssExtractPlugin.loader,
+                    {
+                        loader: "css-loader",
+                        options: { sourceMap: true, importLoaders: 1 },
+                    },
+                    { loader: "sass-loader", options: { sourceMap: true } },
+                ],
             },
-            {
-                test: /\.(woff|woff2|eot|ttf|otf)$/i,
-                type: "asset/resource",
-            },
+            { test: /\.(png|svg|jpg|jpeg|gif)$/i, type: "asset/resource" }, //импорт медиа файлов в js-код
+            { test: /\.(woff|woff2|eot|ttf|otf)$/i, type: "asset/resource" },
         ],
     },
     plugins: [
+        new HtmlWebpackPlugin({
+            template: "./index.html",
+            filename: "index.html",
+        }),
+        new MiniCssExtractPlugin({
+            filename: "style.css",
+        }),
         new CopyPlugin({
             patterns: [{ from: "static", to: "static" }],
         }),
-        new HtmlWebpackPlugin({
-            template: "./src/index.html",
-        }),
     ],
+    optimization: {
+        minimizer: ["...", new CssMinimizerPlugin()],
+        minimize: true,
+    },
+    devtool: isProduction ? "hidden-source-map" : "source-map",
 };
