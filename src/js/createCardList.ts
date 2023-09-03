67506dc77ccbcee990aceb45a2c165b3aee74eaf
Iss1937 <133023027+Iss1937@users.noreply.github.com>
2023-09-03 22:12:18 +0300

    need fix ts

--- src/js/createCardList.ts
+++ src/js/createCardList.ts
@@ -1,19 +1,34 @@
-import { shuffle } from "./help.js";
+import { shuffle } from "./help";
+import { startTimer } from "./timer";
 
-export const createCardList = (cardsContainer, countCard) => {
-    const cardSuit = ["пики", "черви", "бубны", "крести"];
-    const rang = [6, 7, 8, 9, 10, "валет", "дама", "король", "туз"];
-    let gameCards = [];
+export const createCardList = (
+    cardsContainer: HTMLElement,
+    countCard: number,
+    second: HTMLElement,
+    minute: HTMLElement,
+) => {
+    const cardSuit = ["spades", "hearts", "diamonds", "crosses"];
+    const rang = [6, 7, 8, 9, 10, "jack", "queen", "king", "ace"];
+    let gameCards: { [key: string]: string | number }[] = [];
 
     for (let i = 0; i < countCard / 2; i++) {
-        let obj = {};
-        let suit = cardSuit[Math.floor(Math.random() * cardSuit.length)];
-        let card = rang[Math.floor(Math.random() * rang.length)];
+        const obj: { [key: string]: string | number } = {};
+        let suit: string;
+        let card: string | number;
+        do {
+            suit = cardSuit[Math.floor(Math.random() * cardSuit.length)];
+            card = rang[Math.floor(Math.random() * rang.length)];
+        } while (
+            gameCards.some(
+                (item) =>
+                    Object.values(item)[0] === card &&
+                    Object.keys(item)[0] === suit,
+            )
+        );
         obj[suit] = card;
         gameCards.push(obj);
         gameCards.push(obj);
     }
-
     gameCards = shuffle(gameCards);
 
     for (const card of gameCards) {
@@ -41,6 +56,9 @@ export const createCardList = (cardsContainer, countCard) => {
         }
         for (const closeCard of closedCards) {
             closeCard.classList.remove("hidden");
+        }
+        if (second !== null && minute !== null) {
+            startTimer(second, minute);
         }
     }, 3000);
 
---
+++ src/js/startGame.ts
@@ -0,0 +1,106 @@
+import { createCardList } from "./createCardList";
+import { stopTimer } from "./timer";
+import {
+    renderCardGameHeader,
+    renderSuccessModalWindow,
+    renderFailModalWindow,
+} from "./renderComponents";
+
+function finishSuccessGame() {
+    const time = stopTimer();
+    const secondVal = Math.floor(time) - Math.floor(time / 60) * 60;
+    const minuteVal = Math.floor(time / 60);
+    const modalWindow = document.querySelector<HTMLElement>(".cards__modal");
+    setTimeout(() => {
+        if (modalWindow) {
+            renderSuccessModalWindow(modalWindow, secondVal, minuteVal);
+        }
+    }, 500);
+}
+
+function finishFailGame() {
+    const time = stopTimer();
+    const secondVal = Math.floor(time) - Math.floor(time / 60) * 60;
+    const minuteVal = Math.floor(time / 60);
+    const modalWindow = document.querySelector<HTMLElement>(".cards__modal");
+    setTimeout(() => {
+        if (modalWindow) {
+            renderFailModalWindow(modalWindow, secondVal, minuteVal);
+        }
+    }, 500);
+}
+
+export const startGame = (countCard: number) => {
+    let firstCard = -1;
+    let secondCard = -1;
+    let clickable = true;
+
+    const appHtml = document.querySelector<HTMLElement>(".app");
+
+    if (appHtml !== null) {
+        renderCardGameHeader(appHtml, countCard);
+
+        const minute = document.querySelector<HTMLElement>(
+            ".time__value--minutes",
+        );
+        const second = document.querySelector<HTMLElement>(
+            ".time__value--seconds",
+        );
+        const cardsContainer = document.querySelector(".cards__wrapper");
+
+        createCardList(
+            cardsContainer as HTMLElement,
+            countCard,
+            second as HTMLElement,
+            minute as HTMLElement,
+        );
+
+        const cardsList = document.querySelectorAll<HTMLElement>(".cards_item");
+
+        cardsList.forEach((card, index) => {
+            card.addEventListener("click", () => {
+                if (clickable === true && !card.classList.contains("success")) {
+                    card
+                        .querySelector(".card__open-image")
+                        ?.classList.remove("hidden");
+                    card.querySelector(".card__image")?.classList.add("hidden");
+
+                    if (firstCard === -1) {
+                        firstCard = index;
+                    } else if (index !== firstCard) {
+                        secondCard = index;
+                        clickable = false;
+                    }
+
+                    if (
+                        firstCard !== -1 &&
+                        secondCard !== -1 &&
+                        firstCard !== secondCard
+                    ) {
+                        if (
+                            cardsList[firstCard].dataset.card ===
+                            cardsList[secondCard].dataset.card
+                        ) {
+                            setTimeout(() => {
+                                cardsList[firstCard].classList.add("success");
+                                cardsList[secondCard].classList.add("success");
+                                clickable = true;
+                                firstCard = -1;
+                                secondCard = -1;
+                                if (
+                                    Array.from(cardsList).every((card) =>
+                                        card.classList.contains("success"),
+                                    )
+                                ) {
+                                    finishSuccessGame();
+                                }
+                            }, 500);
+                        } else {
+                            finishFailGame();
+                        }
+                    }
+                }
+            });
+        });
+    }
+};
